0672938667c43f4820f8f890fbd53ed114ed9a4d
DanielMashmaniuk <82703760+DanielMashmaniuk@users.noreply.github.com>
2024-06-19 20:37:43 +0300

    Heroku 1.0

--- app/src/main/java/com/example/duetly/Models/MyApi.kt
+++ app/src/main/java/com/example/duetly/Models/MyApi.kt
@@ -1,11 +1,10 @@
 package com.example.duetly.Models
 
-import com.example.duetly.AuthFragment
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface MyApi {
     @POST("register")
-    fun registerUser(@Body request: AuthFragment.RegisterRequest): Call<RegisterResponse>
+    fun registerUser(@Body request: RegisterRequest): Call<RegisterResponse>
 }
